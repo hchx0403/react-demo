630ef05a537f99e9bff98253599218ca1cf85cfb
HUANGCX\COMAC1 <hchx0403@163.com>
2021-02-03 23:06:07 +0800

    add Page 1 2

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import './App.css';
 import Page1 from './components/Page1'
+import Page2 from './components/Page2'
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+
 function App() {
   return (
-    <div className="App">
+  
+    <Router>
+      <Switch>
+        <Route path="/Page1" exact={true} component={Page1} />
+        <Route path="/Page2" exact={true} component={Page2} />
+      </Switch>
+    </Router>
 
-      <Page1 />
-
-    </div>
+    
   );
 }
 
 export default App;
+
+
